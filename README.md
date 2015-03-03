fa43aa52908fa7dc5945a1b7cddea6ad457ef976
Michael Gilley <michaelgilley@gmail.com>
2015-03-02 16:12:44 -0800

    Add some functionality

--- README.md
+++ README.md
@@ -15,7 +15,18 @@ npm install barth -S
 ```
 
 ## Usage
+```barth([num [, options]])```
+
 ```
 var barth = require('barth')
 
-barth()
+barth() // => {text: 'A quote.'}
+barth(3) // => [{text: 'An array of 3 quotes.'},{...},{...}]
+barth({textOnly: true}) // => 'Only the quote text will be returned.'
+```
+```barth.daily([options])```
+
+```
+barth.daily() // => '{text: 'A quote that will show all day long.}'
+barth.daily({textOnly: true}) // => 'A quote that shows all day long.'
+```
--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 
 var quotes = require('./data').quotes
 
-function getAQuote () {
-  var pick = Math.floor(Math.random() * quotes.length)
+function getAQuote (idx) {
+  var pick = idx || Math.floor(Math.random() * quotes.length)
   return quotes[pick]
 }
 
-module.exports = function(num) {
+function formatter (quote, opts) {
+  if (opts.textOnly) return quote.text
+  return quote
+}
+
+module.exports = exports = function(num, opts) {
+  if (!opts && typeof num === 'object') {
+    opts = num
+    num = 1
+  }
   num = Math.floor(+num || 1)
-  if (num === 1) return getAQuote()
+  opts = Object(opts)
+  if (num === 1) return formatter(getAQuote(), opts)
   var quotes = []
   for (var i = 1; i <= num; i++) {
-    quotes.push(getAQuote())
+    quotes.push(formatter(getAQuote(), opts))
   }
   return quotes
+}
+
+exports.daily = function(opts) {
+  opts = Object(opts)
+
+  var now = new Date()
+    , beginningOfDay = (new Date(now.getFullYear(), now.getMonth(), now.getDate())) / 1000
+
+  return formatter(getAQuote(beginningOfDay % quotes.length), opts)
 }
